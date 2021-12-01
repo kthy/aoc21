1d3b23e6520160e0ccdcce5b5c50ad794a70036d
Kristian Thy <XKTHY@orsted.dk>
2021-12-01 10:20:06 +0100

    feat(01): full Dec 01 solution in Python

--- 01/01.py
+++ 01/01.py
@@ -1,11 +1,45 @@
+from dataclasses import dataclass
+from typing import Sequence
+
+INFINITY = float("inf")
+
+
+@dataclass
+class SlidingWindow:
+    old: int
+    mid: int
+    new: int
+
+    def slide(self, new_new: int) -> None:
+        self.old = self.mid
+        self.mid = self.new
+        self.new = new_new
+
+    def sum(self) -> int:
+        w = self.window()
+        return sum(w) if all(w) else INFINITY
+
+    def window(self) -> Sequence[int]:
+        return [self.old, self.mid, self.new]
+
+
 if __name__ == "__main__":
     counter = 0
-    last_depth = float('inf')
+    sliding_counter = 0
+    last_depth = INFINITY
+    sliding_window = SlidingWindow(INFINITY, INFINITY, INFINITY)
+    last_sliding_depth = INFINITY
     with open("./input/depths.txt") as depths:
         for line in depths.readlines():
             depth = int(line)
             if depth > last_depth:
                 counter += 1
+            sliding_window.slide(depth)
+            sliding_depth = sliding_window.sum()
+            if sliding_depth > last_sliding_depth:
+                sliding_counter += 1
             last_depth = depth
+            last_sliding_depth = sliding_depth
 
-    print(counter)
+    print(f"{counter} measurements are larger than the previous measurement.")
+    print(f"{sliding_counter} three-measurement sliding window sums are larger than the previous sum.")
